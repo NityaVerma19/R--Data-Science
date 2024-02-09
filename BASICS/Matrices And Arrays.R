97a30038783aef8edbc4cde62b4a1765bf65a1c0
Nitya Verma <vrmnitya@gmail.com>
2024-02-09 07:43:08 +0530

    use rbind and cbind

--- BASICS/Matrices And Arrays.R
+++ BASICS/Matrices And Arrays.R
@@ -2,3 +2,24 @@
 
 a = matrix(data = c(-3,2,893,0.17), nrow = 2, ncol=2)
 a
+
+
+#byrow
+matrix(data=c(1,2,3,4,5,6),nrow=2,ncol=3,byrow=FALSE)
+
+matrix(data=c(1,2,3,4,5,6),nrow=2,ncol=3,byrow=TRUE)
+
+
+
+#------------------------row and column bindings--------------------------------#
+
+#If you have multiple vectors of equal length, you can quickly build a matrix
+#by binding together these vectors using the built-in R functions, rbind and
+#cbind.
+
+
+rbind(1:3, 4:6)
+
+cbind(c(1,4), c(2,5), c(3,6))
+
+
