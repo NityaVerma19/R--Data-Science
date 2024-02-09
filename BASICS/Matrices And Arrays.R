cf015cf9c5321263dbf14d9ad7cf323a0e450150
Nitya Verma <vrmnitya@gmail.com>
2024-02-09 08:20:07 +0530

    omit

--- BASICS/Matrices And Arrays.R
+++ BASICS/Matrices And Arrays.R
@@ -63,3 +63,76 @@ A[,c(3,1)]
 #accesses the third and first rows of A, in that order, and from those rows it
 #returns the second and third column elements.
 A[c(3,1),2:3]
+
+
+
+#-----Omitting and Overwriting
+
+
+#To delete or omit elements from a matrix, you again use square brackets,
+#but this time with negative indexes
+
+A
+A[,-2]  #omitting 2nd col
+
+
+#Removing the first row and extracting 3rd and 2nd col
+A
+A[-1, c(3,2)]
+
+
+#Removing 1st row and 2nd col
+
+A[-1,-2]
+
+
+
+#To overwrite particular elements, or entire rows or columns, you identify
+#the elements to be replaced and then assign the new values
+
+B = A
+B
+
+# overwrites the second row of B with the sequence 1, 2,and 3
+B[2,] = 1:3
+B
+
+
+#overwrites the second column elements of the first andthird rows with 900
+
+B[c(1,3), 2] = 900
+B
+
+
+
+# replace the diagonal of a square matrix
+
+diag(x =B) = rep(x = 0, times = 3)
+B
+
+
+#-------------------------------------------EXERCISE 3.1---------------------#
+
+#a) 
+
+C = matrix(c(4.3, 3.1, 8.2, 8.2, 3.2, 0.9, 1.6,6.5), nrow = 4, ncol = 2)
+C
+
+#b) 
+B = C[-1,]
+dim(B)
+
+#c) 
+C
+C[,2] = sort(C[,2])
+C
+
+
+#d) 
+ 
+matrix(C[-4,-1])
+
+
+#e)
+C
+matrix(data = C[c(3,4),c(1,2)], nrow =2, ncol = 2)
